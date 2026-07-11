9c617611f2af77153628c73300fbebe00ddf6494
Lu.Bu <170906249+bunpmc@users.noreply.github.com>
2026-07-11 20:03:38 +0700

    Increase HTTP server stack size to 16KB

--- CameraWebServer/app_httpd.cpp
+++ CameraWebServer/app_httpd.cpp
@@ -704,6 +704,7 @@ static esp_err_t play_handler(httpd_req_t *req) {
 void startCameraServer() {
   httpd_config_t config = HTTPD_DEFAULT_CONFIG();
   config.max_uri_handlers = 16;
+  config.stack_size = 16384;
 
   httpd_uri_t index_uri = {
     .uri = "/",
